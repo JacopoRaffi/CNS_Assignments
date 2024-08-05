50752894bec6a7eaf80ccb3808eadb956ce7c77a
JacopoRaffi <raffijacopo@gmail.com>
2024-08-05 10:25:17 +0200

    add comments and clean readme

--- LAB2_1/learning.py
+++ LAB2_1/learning.py
@@ -1,34 +1,96 @@
 import numpy as np
 
 class ABCRule:
+    '''
+    Abstract Class for the learning rules
+    
+    Attributes
+    ----------
+    w : numpy array
+        weights of the neuron
+    lr : float
+        learning rate for training
+    '''
+
     def __init__(self, input_size:int, lr:float = 1e-4):
+        '''
+        Parameters
+        ---------
+        input_size : int
+            size of the input vector
+        lr : float
+            learning rate for training
+        '''
+
         RANDOM_STATE = 42
         rand_gen = np.random.default_rng(RANDOM_STATE)
         self.w = rand_gen.uniform(-1, 1, input_size) # input is 2-dimension
         self.lr = lr
 
     def predict(self, u:np.ndarray) -> np.ndarray:
+        '''
+        Compute the output of the neuron
+
+        Parameters
+        ---------
+        u : numpy array
+            input vector
+        '''
+
         return np.inner(self.w, u)
 
     def update(self, u:np.ndarray, v:np.ndarray) -> np.ndarray:
+        '''
+        Update the weights of the neuron (every sub-class needs to implement this method)
+
+        Parameters
+        ---------
+        u : numpy array
+            input vector
+        v : numpy array
+            output of the neuron
+
+        Returns
+        -------
+        return: -
+        '''
         pass
 
 
 def train(rule:ABCRule, data:np.ndarray, 
           threshold:float = 1e-5, epochs:int = 100) -> tuple:
+    '''
+    Train the neuron with the given rule
+
+    Parameters
+    ---------
+    rule : ABCRule
+        learning rule to use
+    data : numpy array
+        dataset
+    threshold : float
+        threshold used to stop the training loop confronted with the norm of the old and new weights
+    epochs : int
+        max number of epochs
+
+    Returns
+    -------
+    return: tuple
+        tuple containing the history of the weights vector and the norm of the weights vector over the epochs
+    '''
    
     norm_distance = np.inf
-    w_history = [rule.w.copy()]
+    w_history = [rule.w.copy()] 
     norm_history = []
     epoch = 0
 
     while (epoch < epochs) and (norm_distance > threshold):
         np.random.shuffle(data) # shuffle data at each epoch
-        w_old = rule.w.copy() # copy the old wiehts to compare with the new using the norm2
+        w_old = rule.w.copy() # copy the old weights to compare with the new using the norm 2
 
         for u in data:
-            v = rule.predict(u)
-            rule.update(u, v)
+            v = rule.predict(u) 
+            rule.update(u, v) 
 
             w_history.append(rule.w.copy())
             norm_history.append(np.linalg.norm(rule.w))
@@ -39,17 +101,36 @@ def train(rule:ABCRule, data:np.ndarray,
     return w_history, norm_history
 
 
-# --------------------------------- Implementation of different Rules --------------------------------- #
+# --------------------------------- Implementation of different Update Rules --------------------------------- #
 class HebbRule(ABCRule):
+    '''
+    Basic Hebb Learning Rule
+
+    Attributes
+    ----------
+    same as ABCRule
+    '''
+    
     def __init__(self, input_size:int, lr:float = 1e-4):
         super().__init__(input_size, lr)
 
     def update(self, u:np.ndarray, v:np.ndarray) :
-        delta_w = self.lr * v * u # compute the delta W
-        self.w += delta_w # update the weights
+        delta_w = self.lr * v * u 
+        self.w += delta_w 
 
 
 class OjaRule(ABCRule):
+    '''
+    Basic Hebb Learning Rule
+
+    Attributes
+    ----------
+    same as ABCRule
+
+    alpha : float
+        hyperparameter for the Oja rule
+    '''
+    
     def __init__(self, input_size:int, lr:float = 1e-4, alpha:float = 2):
         super().__init__(input_size, lr)
         self.alpha = alpha
@@ -59,6 +140,14 @@ class OjaRule(ABCRule):
         self.w += delta_w
 
 class SNRule(ABCRule):
+    '''
+    Subtractive Normalization Learning Rule
+
+    Attributes
+    ----------
+    same as ABCRule
+    '''
+    
     def __init__(self, input_size:int, lr:float = 1e-4):
         super().__init__(input_size, lr)
 
@@ -70,6 +159,17 @@ class SNRule(ABCRule):
         self.w += delta_w
 
 class CovarianceRule(ABCRule):
+    '''
+    Covariance Learning Rule
+
+    Attributes
+    ----------
+    same as ABCRule
+
+    theta : float
+        threshold on the presynaptic rate
+    '''
+    
     def __init__(self, input_size:int, lr:float = 1e-4, theta:float = 1):
         super().__init__(input_size, lr)
         self.theta = theta
@@ -79,12 +179,38 @@ class CovarianceRule(ABCRule):
         self.w += delta_w
 
 class BCMRule(ABCRule):
+    '''
+    BCM Learning Rule
+
+    Attributes
+    ----------
+    same as ABCRule
+
+    theta : float
+        varying threshold with its own update 
+    theta_lr : float
+        learning rate for the threshold
+    '''
+    
     def __init__(self, input_size:int, lr:float = 1e-4, theta:float = 0.1, theta_lr:float = 0.001):
         super().__init__(input_size, lr)
         self.theta = theta  
         self.theta_lr = theta_lr
 
     def __theta_update(self, v:np.ndarray):
+        '''
+        Update the threshold theta
+
+        Parameters
+        ---------
+        v : numpy array
+            output of the neuron
+
+        Returns
+        -------
+        return: -
+        '''
+        
         self.theta += self.theta_lr * (np.power(v, 2) - self.theta)
 
     def update(self, u: np.ndarray, v: np.ndarray) -> np.ndarray:
--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # CNS_Assignments
 Laboratory ssignments given during the Computational NeuroScience(CNS) course.
 
-## Assignment 1
-1. The assignment consists of the following points:
-
-2. Implement the Izhikevich’s model in Matlab
-Develop all the 20 neuro-computational features of biological neurons using the model developed at point 1) and plot: 
-    - the resulting membrane potential’s time courses into individual figures (one figure for each neuro-computational feature);
-    - the phase portraits that correspond to each of the neuro-computational features (one figure for each neuro-computational feature).
