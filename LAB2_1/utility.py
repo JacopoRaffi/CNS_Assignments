7d9cec855e8fea98dfba0e6ac6bf6d46a56abe8e
JacopoRaffi <raffijacopo@gmail.com>
2024-08-05 09:54:02 +0200

    add comments

--- LAB2_1/utility.py
+++ LAB2_1/utility.py
@@ -2,19 +2,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def eig_plot(data:np.ndarray, weights:np.ndarray):
+    '''
+    Plot the principal eigenvector, the weights vector and the dataset
+
+    Parameters
+    ---------
+    data: numpy array
+         dataset 
+    w: numpy array
+        final weghts vector
+
+    Returns
+    -------
+    return: -
+
+    '''
+        
     Q = np.corrcoef(data.T)
     eig_values, eig_vecs = np.linalg.eig(Q)
-    i_max_eigval = np.argsort(eig_values)[-1]
+    idx_max_eigval = np.argsort(eig_values)[-1]
     origin = np.array([0,0])
     
     plt.scatter(data[:, 0], data[:, 1])
     plt.quiver(*origin, weights[0], weights[1], color='g', label='weights')
-    plt.quiver(*origin, eig_vecs[:, i_max_eigval][0], eig_vecs[:, i_max_eigval][1],  color='r', label='principal eigenvector', scale=10)
+    plt.quiver(*origin, eig_vecs[:, idx_max_eigval][0], eig_vecs[:, idx_max_eigval][1],  color='r', label='principal eigenvector', scale=10)
     plt.legend()
 
     plt.show()
 
-def weights_plot(w_history:np.ndarray, norm_history:np.ndarray):
+def weights_plot(w_history:list, norm_history:list):
+    '''
+    Plot the evolution of the weights and the norm of the weights over the epochs
+    
+    Parameters
+    ---------
+    w_history: list
+        list of the weights vector at each epoch
+    norm_history: list
+        list of the norm of the weights vector at each epoch
+
+    Returns
+    -------
+    return: -
+    
+    '''
+
     fig, axs = plt.subplots(1, 3, figsize=(25,6))
 
     axs[0].plot(np.array(w_history)[:, 0], color='r')
@@ -25,7 +57,7 @@ def weights_plot(w_history:np.ndarray, norm_history:np.ndarray):
     axs[1].set_title('Weight 2 Evolution')
     axs[1].set_xlabel('Epochs')
 
-    axs[2].plot(np.power(norm_history, 2))
+    axs[2].plot(norm_history, 2)
     axs[2].set_title('Weights Norm Evolution')
     axs[2].set_xlabel('Epochs')
 
