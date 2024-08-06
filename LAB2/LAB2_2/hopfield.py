e97037015c391bbc6cf4789f7bc3c2d227d54b06
JacopoRaffi <raffijacopo@gmail.com>
2024-08-06 10:50:01 +0200

    add incremental update and comments

--- LAB2/LAB2_2/hopfield.py
+++ LAB2/LAB2_2/hopfield.py
@@ -1,13 +1,50 @@
 import numpy as np
 
 class HopfieldNetwork:
-    def __init__(self, data:np.ndarray) -> None:
+    '''
+    Implementation of the Hopfield Network
+
+    Attributes:
+    ----------
+    data: np.ndarray
+        Patterns to be stored in the network
+    n_patterns: int
+        Number of patterns
+    size: int
+        number of neurons
+    '''
+
+    def __init__(self, data:np.ndarray):
+        '''
+        Parameters:
+        -----------
+        data: np.ndarray
+            Patterns to be stored in the network
+
+        Returns:
+        --------
+        return: -
+        '''
         self.n_patterns, self.size = data.shape
         self.W = self.__learning(data)
         self.patterns = data
         
 
-    def __learning(self, data:np.ndarray) -> None:
+    def __learning(self, data:np.ndarray):
+        '''
+        Learning the weights of the network
+
+        Parameters:
+        -----------
+        data: np.ndarray
+            Patterns to be stored in the network
+        
+        Returns:
+        --------
+        return: np.ndarray
+            Weight matrix of the network
+        '''
+
         W = np.zeros((self.size, self.size))
 
         for pattern in data:
@@ -18,18 +55,59 @@ class HopfieldNetwork:
         return (W / self.size)
 
     def __energy(self, state:np.ndarray) -> float:
+        '''
+        Compute the energy of the network
+
+        Parameters:
+        -----------
+            state: np.ndarray
+                State of the network's neurons
+        
+        Returns:
+        --------
+            return: float
+                Energy of the network
+        '''
+        
         return -0.5 * (np.dot(np.dot(self.W, state), state))
 
     def __overlap(self, state:np.ndarray, true_pattern:np.ndarray) -> float:
+        '''
+        Compute the overlap of the network
+
+        Parameters:
+        -----------
+        state: np.ndarray
+            State of the network's neurons
+
+        true_pattern: np.ndarray
+            Target Pattern
+        
+        Returns:
+        --------
+        return: float
+            Overlap of the network
+        '''
         return (1/self.size) * np.dot(state, true_pattern)
+    
+    def add_pattern(self, pattern:np.ndarray):
+        '''
+        Add a new pattern to the network
 
-    def __call__(self, input:np.ndarray, steps:int = 10, bias = 0.6, 
-                 history:bool = False, true_pattern:np.ndarray = None) -> np.ndarray:
+        Parameters:
+        -----------
+        pattern: np.ndarray
+            New pattern to be added
+
+        Returns:
+        --------
+        return: -
+        '''
+        self.W += (np.outer(pattern, pattern)) / self.size # incremental update of the weights
+
+    def __call__(self, input:np.ndarray, steps:int = 10, bias = 0.6, true_pattern:np.ndarray = None) -> tuple:
+        
         state = input.copy()
-
-        if history:
-            assert true_pattern is not None, "True pattern is required for overlap history"
-
         energy_history, overlap_history = [], []
         not_converged = True
         step = 0
@@ -42,9 +120,8 @@ class HopfieldNetwork:
                 output = np.dot(self.W[neuron], state) + bias
                 state[neuron] = np.where(output > 0, 1, -1)
 
-                if history:
-                    energy_history.append(self.__energy(state))
-                    overlap_history.append(self.__overlap(state, true_pattern))
+                energy_history.append(self.__energy(state))
+                overlap_history.append(self.__overlap(state, true_pattern))
             
             step += 1
             not_converged = not np.array_equal(state, previous_state)
