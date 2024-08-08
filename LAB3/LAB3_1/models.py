50d8ec516c5da3242598abb8c025329450696fb5
JacopoRaffi <raffijacopo@gmail.com>
2024-08-08 18:21:44 +0200

    implemented VanillaRNN

--- LAB3/LAB3_1/models.py
+++ LAB3/LAB3_1/models.py
@@ -7,6 +7,7 @@ class TDNN(nn.Module):
     Implementation of a TDNN model (multilayer perceptron with tanh activation function for the hidden layer)
 
     Attributes:
+    ----------
     window_size: int
         The size of the input window
     hidden_layer: nn.Linear
@@ -52,3 +53,57 @@ class TDNN(nn.Module):
         x = self.output_layer(x)
         
         return x
+    
+
+class VanillaRNN(nn.Module):
+    '''
+    Implementation of a vanilla RNN (for regression task)
+
+    Attributes:
+    ----------
+    rnn: torch.nn.Module
+        recurrent layer of the vanilla model
+    readout: torch.nn.Module
+        output layer of the model
+    '''
+    def __init__(self, input_size:int, hidden_size:int, output_size:int = 1):
+        '''
+        Initialize the VanillaRNN model
+
+        Parameters:
+        ----------
+        input_size: int
+            The size of the input
+        hidden_size: int
+            The number of features in the hidden state h
+        output_size: int
+            The size of the output layer (readout) 
+
+        Returns:
+        -------
+        return: -
+        '''
+        super(VanillaRNN, self).__init__()
+
+        self.rnn = nn.RNN(input_size=input_size, hidden_size=hidden_size)
+        self.readout = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, h0):
+        '''
+        Forward pass of the model
+
+        Parameters:
+        ----------
+        x: torch.Tensor
+            The input sequence
+        h0: torch.Tensor
+            The initial hidden state of the recurrent layer
+
+        Returns:
+        -------
+        out: tuple
+            the predicted output of the model and the last hidden state of the recurrent layer
+        '''
+        out, hn = self.rnn(x, h0)
+
+        return self.readout(out), hn
