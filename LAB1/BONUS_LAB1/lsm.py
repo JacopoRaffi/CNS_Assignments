fcbb4d672e3c5785271de98490aa087775727bd0
JacopoRaffi <raffijacopo@gmail.com>
2024-08-08 10:21:08 +0200

    add comments

--- LAB1/BONUS_LAB1/lsm.py
+++ LAB1/BONUS_LAB1/lsm.py
@@ -1,7 +1,49 @@
 import numpy as np
 
 class LSM:
+    '''
+    Implementation of Liquid State Machine
+
+    Attributes:
+    ----------
+    Ne: int
+        Number of excitatory neurons
+    Ni: int
+        Number of inhibitory neurons
+    a: np.ndarray
+        Time scale of the recovery variable u
+    b: np.ndarray
+        Sensitivity of the recovery variable u to the subthreshold fluctuations of the membrane potential v
+    c: np.ndarray
+        After-spike reset value of the membrane potential v
+    d: np.ndarray
+        After-spike reset of the recovery variable u
+    U: np.ndarray
+        Scaling of input connections
+    S: np.ndarray
+        Scaling of recurrent connections
+    Wout: np.ndarray
+        Output weights
+    '''
     def __init__(self, Ne, Ni, win_e, win_i, w_e, w_i):
+        '''
+        Initialize the Liquid State Machine
+
+        Parameters:
+        ----------
+        Ne: int
+            Number of excitatory neurons
+        Ni: int
+            Number of inhibitory neurons
+        win_e: float
+            Scaling of input connections for excitatory neurons
+        win_i: float
+            Scaling of input connections for inhibitory neurons
+        w_e: float
+            Scaling of recurrent connections for excitatory neurons
+        w_i: float
+            Scaling of recurrent connections for inhibitory neurons
+        '''
         self.Ne = Ne
         self.Ni = Ni
         
@@ -21,15 +63,26 @@ class LSM:
         self.Wout = None
 
     def __compute_states(self, input:np.ndarray):
+        '''
+        Compute the reservoir states
+
+        Parameters:
+        ----------
+        input: np.ndarray
+            Input data
+        
+        Returns:
+        --------
+        return: np.ndarray
+            Return the reservoir states
+        '''
         v = -65*np.ones(self.Ne + self.Ni)  # Initial values of v
         u = self.b*v  # Initial values of u
-        #firings = []  # spike timings
         states = []  # here we construct the matrix of reservoir states
 
         for t in range(len(input)): 
             I = input[t] * self.U
             fired = np.where(v >= 30)[0]  # indices of spikes
-            #firings.append(np.column_stack((t + np.zeros_like(fired), fired)))
             v[fired] = self.c[fired]
             u[fired] = u[fired] + self.d[fired]
             I = I + np.sum(self.S[:, fired], axis=1)
@@ -38,17 +91,41 @@ class LSM:
             u = u + self.a*(self.b*v - u)
             states.append(v >= 30)
 
-        #firings = np.concatenate(firings)
-
         return np.array(states, dtype=int)
 
-    def fit(self, input:np.ndarray, target:np.ndarray, lamd:float = 0.1):
+    def fit(self, input:np.ndarray, target:np.ndarray):
+        '''
+        Fit the output weights (Wout)
+
+        Parameters:
+        ----------
+        input: np.ndarray
+            Input data
+        target: np.ndarray
+            Target data
+        
+        Returns:
+        --------
+        return: -
+        '''
         states = self.__compute_states(input)
 
-        # Tikhonov regularization is applied
         self.Wout = np.linalg.pinv(states) @ target
 
     def __call__(self, input:np.ndarray):
+        '''
+        Predict the target values
+
+        Parameters:
+        ----------
+        input: np.ndarray
+            Input data
+        
+        Returns:
+        --------
+        return: np.ndarray
+            Return the predicted target values
+        '''
         states = self.__compute_states(input)
 
         return (states @ self.Wout)
--- LAB1/BONUS_LAB1/utility.py
+++ LAB1/BONUS_LAB1/utility.py
@@ -3,10 +3,48 @@ import itertools
 from lsm import LSM
 
 def MAE(Y, Y_hat):
+    '''
+    Compute Mean Absolute Error
+
+    Parameters:
+    ----------
+    Y: np.ndarray
+        Target values
+    Y_hat: np.ndarray
+        Predicted values
+
+    Returns:
+    --------
+    return: float
+        Return the Mean Absolute Error
+    '''
     return np.mean(np.abs(Y - Y_hat))
 
 
 def grid_search(hyperparameters:dict, train_X:np.ndarray, train_Y:np.ndarray, val_X:np.ndarray, val_Y:np.ndarray):
+    '''
+    Perform grid search to find the best hyperparameters
+
+    Parameters:
+    ----------
+    hyperparameters: dict
+        Dictionary containing hyperparameters to be tuned
+    train_X: np.ndarray
+        Training input data
+    train_Y: np.ndarray
+        Training target
+    val_X: np.ndarray
+        Validation input data
+    val_Y: np.ndarray
+        Validation target
+
+    Returns:
+    --------
+    return: tuple
+        Return the best hyperparameters, best validation MAE and the corresponding training MAE
+    '''
+
+    # create a list of dict contaning all possible configurations
     all_configs = [dict(zip(hyperparameters.keys(), config)) for config in itertools.product(*hyperparameters.values())]
 
     best_config = None
@@ -19,7 +57,7 @@ def grid_search(hyperparameters:dict, train_X:np.ndarray, train_Y:np.ndarray, va
         Y_hat = lsm(val_X)
         mae = MAE(val_Y, Y_hat)
         
-        if mae < best_MAE:
+        if mae < best_MAE: # update best configuration
             best_MAE = mae
             best_config = config.copy()
             y_train_hat = lsm(train_X)
