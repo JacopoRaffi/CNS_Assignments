928e1988224fa830b962b923dfcd58a6deff77e4
JacopoRaffi <raffijacopo@gmail.com>
2024-08-16 16:53:25 +0200

    fix train mse

--- LAB3/LAB3_2/learning.py
+++ LAB3/LAB3_2/learning.py
@@ -24,12 +24,12 @@ def grid_search(hyperparameters:dict, train_x, train_y, val_x, val_y, n_iter:int
         washout = config['washout']
         for _ in range(n_iter):
             esn = RegressorESN(input_size=input_size, hidden_size=config['hidden_size'], ridge_regression=config['ridge_regression'],
-                            omhega_in=config['omhega_in'], omhega_b=config['omhega_b'], rho=config['rho'], density=1)
+                            omhega_in=config['omhega_in'], omhega_b=config['omhega_b'], rho=config['rho'])
 
             esn.train()        
             h_last = esn.fit(train_x, train_y, washout)
             train_pred = esn(train_x, None)
-            train_mse.append(mse(train_pred, train_y).item())
+            train_mse.append(mse(train_pred[washout:], train_y[washout:]).item())
             
             esn.eval()
             val_pred = esn(val_x, h_init=h_last)
