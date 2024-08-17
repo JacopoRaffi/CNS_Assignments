5d79d7fa84d29456e1f39233f3d8a02115b993a0
JacopoRaffi <raffijacopo@gmail.com>
2024-08-17 17:52:35 +0200

    completed bonus task (very funny)

--- LAB3/BONUS_LAB3/arnn.py
+++ LAB3/BONUS_LAB3/arnn.py
@@ -3,43 +3,16 @@ import math
 import torch.nn as nn
 import torch.nn.functional as F
 
-class AntisymmetricRNN(nn.Module): 
-    def __init__(self, input_size, hidden_size, euler_step:float, diffusion:float):
-        super(AntisymmetricRNN, self).__init__()
-        
-        self.input_size = nn.Parameter(torch.tensor(input_size), requires_grad=False)
-        self.hidden_size = nn.Parameter(torch.tensor(hidden_size), requires_grad=False)
-        self.euler_step = nn.Parameter(torch.tensor(euler_step), requires_grad=False)
-        self.diffusion = nn.Parameter(torch.tensor(diffusion), requires_grad=False)
-
-        sqrt_k = 1 / math.sqrt(hidden_size)
-        
-        # weight initializitation equal to the initialization of RNN in PyTorch (from Uniform(-sqrt(k), sqrt(k)))
-        self.W_in = nn.Parameter(nn.init.uniform_(torch.empty(hidden_size, input_size), -sqrt_k, sqrt_k), requires_grad=True)
-        self.W_h = nn.Parameter(nn.init.uniform_(torch.empty(hidden_size, hidden_size), -sqrt_k, sqrt_k), requires_grad=True)
-
-        self.bias = nn.Parameter(nn.init.uniform_(torch.empty(hidden_size), -sqrt_k, sqrt_k), requires_grad=True)
-
-    def forward(self, input:torch.Tensor, h_init:torch.Tensor):
-        h = torch.zeros(self.hidden_size) if h_init is None else h_init
-        diffusion_eye = torch.eye(self.hidden_size).to(input.device) * self.diffusion
-
-        for x in input:
-            h = h + self.euler_step * (F.linear(h, (self.W_h - self.W_h.T - diffusion_eye)) + F.linear(x, self.W_in, self.bias))
-
-        return h # return only the last hidden state
-
 class CharRNN(nn.Module):
-    def __init__(self, hidden_size, vocab_size, embedding_dim, euler_step:float, diffusion:float):
+    def __init__(self, input_size:int, hidden_size:int, vocab_size:int):
         super(CharRNN, self).__init__()
 
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.rnn = AntisymmetricRNN(embedding_dim, hidden_size, euler_step, diffusion)
+        self.rnn = nn.GRU(input_size, hidden_size)
         self.fc = nn.Linear(hidden_size, vocab_size)
 
     def forward(self, input:torch.Tensor, h_init:torch.Tensor):
-        x = self.embedding(input)
-        h = self.rnn(x, h_init)
+        _, h = self.rnn(input, h_init)
+        print(h.shape)
         out = self.fc(h)
 
-        return out
+        return out, h
